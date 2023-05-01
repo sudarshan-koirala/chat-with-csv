6cbbdcef10f590bb6c8697ca1a25daa8b43445fe
Sudarshan Koirala <snkoirala45@gmail.com>
2023-05-01 13:09:53 +0000

    python file with function to read csv and interact with llm to get answer

---
+++ utils.py
@@ -0,0 +1,37 @@
+# type: ignore
+import os
+from typing import TextIO
+
+import openai
+import pandas as pd
+import streamlit as st
+from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
+from langchain.llms import OpenAI
+
+#openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+
+def get_answer_csv(file: TextIO, query: str) -> str:
+    """
+    Returns the answer to the given query by querying a CSV file.
+
+    Args:
+    - file (str): the file path to the CSV file to query.
+    - query (str): the question to ask the agent.
+
+    Returns:
+    - answer (str): the answer to the query from the CSV file.
+    """
+    # Load the CSV file as a Pandas dataframe
+    # df = pd.read_csv(file)
+    #df = pd.read_csv("titanic.csv")
+
+    # Create an agent using OpenAI and the Pandas dataframe
+    agent = create_csv_agent(OpenAI(temperature=0), file, verbose=False)
+    #agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False)
+
+    # Run the agent on the given query and return the answer
+    #query = "whats the square root of the average age?"
+    answer = agent.run(query)
+    return answer
+``
